b2d64438d8be196f8c5ce169639556e0ca492aac
xecus <naisyo@naisyo.jp>
2014-09-01 19:28:34 +0900

    Update Make.sh

--- Make.sh
+++ Make.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-g++ -m32  -I/usr/local/cuda-5.0/include -I ../nvcc_test/inc -o main.o -c sle_solver_gpu.cpp
-g++ -m32 -o sle_solver_gpu main.o -L/usr/local/cuda-5.0/lib -lcudart -lcublas -lcusparse
+g++ -m32  -I/usr/local/cuda-5.0/include -I ../nvcc_test/inc -o sle_solver_gpu.o -c sle_solver_gpu.cpp
+g++ -m32 -o main.o -c main.cpp
+g++ -m32 -o sle_solver_gpu sle_solver_gpu.o hoge.o -L/usr/local/cuda-5.0/lib -lcudart -lcublas -lcusparse
